8be7a0bd749cadec9a8b4fad7887efee86874b49
nisan250 <nisan250@gmail.com>
2018-11-01 14:41:04 +0000

    Updates

---
+++ precache-manifest.7647ad7ab9e7599d27a2b3700e37183d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8aefb381205bd329218",
+    "url": "/jobs-react.git/static/js/runtime~main.a8aefb38.js"
+  },
+  {
+    "revision": "8fc7040f038f43553c17",
+    "url": "/jobs-react.git/static/js/main.8fc7040f.chunk.js"
+  },
+  {
+    "revision": "6bf90c66beac6e577e69",
+    "url": "/jobs-react.git/static/js/1.6bf90c66.chunk.js"
+  },
+  {
+    "revision": "8fc7040f038f43553c17",
+    "url": "/jobs-react.git/static/css/main.aa4d8e3f.chunk.css"
+  },
+  {
+    "revision": "48d2b185f2a966358dbce18f131f447f",
+    "url": "/jobs-react.git/index.html"
+  }
+];
